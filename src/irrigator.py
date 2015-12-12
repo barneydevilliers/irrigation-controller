f437a4599f318c2681132bc67a91ef27789b29a6
Barney de Villiers <barney.devilliers@gmail.com>
2015-12-12 22:39:35 +0200

    Fundamental fix that daemon do not try to write to ttys

--- src/irrigator.py
+++ src/irrigator.py
@@ -304,8 +304,8 @@ def threadJobScheduler():
 class ServiceDaemon():
     def __init__(self):
         self.stdin_path = '/dev/null'
-        self.stdout_path = '/dev/tty'
-        self.stderr_path = '/dev/tty'
+        self.stdout_path = '/dev/null'
+        self.stderr_path = '/dev/null'
         self.pidfile_path =  '/tmp/irrigator.pid'
         self.pidfile_timeout = 5
 
